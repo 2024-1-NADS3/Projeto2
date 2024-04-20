1a4626e4db9e48a99df6f49a6252e9b91d0f121a
Fernanda Mayumi <128170082+Mayumikkato@users.noreply.github.com>
2024-04-20 10:13:34 -0300

    Commit Arquivo Atualizado com Home

---
+++ Telas/app/src/main/java/com/example/telas_iniciais/Home.java
@@ -0,0 +1,38 @@
+package com.example.telas_iniciais;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
+
+import java.util.ArrayList;
+
+public class Home extends AppCompatActivity {
+
+    private ImageSlider imageSlider;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        imageSlider = findViewById(R.id.image_slider);
+
+        //lista de imagem
+
+        ArrayList<SlideModel> slideModels = new ArrayList<>();
+
+        slideModels.add(new SlideModel(R.drawable.image1, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.image2, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.image13, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.image4, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.image12, ScaleTypes.FIT));
+
+        imageSlider.setImageList(slideModels, ScaleTypes.FIT);
+
+
+
+    }
+}
