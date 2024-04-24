e8af17fcc4ea5332181c35046f3e4055ea73b8dc
Fernanda Mayumi <128170082+Mayumikkato@users.noreply.github.com>
2024-04-24 14:43:47 -0300

    Commit - TelaLogin-TelaCadastro-conexaoServidor

--- Telas/app/src/main/java/com/example/telas_iniciais/CadastroActivity.java
+++ Telas/app/src/main/java/com/example/telas_iniciais/CadastroActivity.java
@@ -7,11 +7,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.EditText;
+
+import okhttp3.*;
+import android.widget.Toast;
+import java.io.IOException;
+
 
 public class CadastroActivity extends AppCompatActivity {
 
     private static final long DELAY_TIME_MS = 3000;
+    private final OkHttpClient usuario = new OkHttpClient();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +30,8 @@ public class CadastroActivity extends AppCompatActivity {
         startActivity(voltarTelaLogin);
     }
 
-    public void BotaoCadastro(View view){
-        TextView inputNomeCadastro, inputEmailCadastro, inputSenhaCadastro;
+    public void BotaoCadastro(View view) {
+        EditText inputNomeCadastro, inputEmailCadastro, inputSenhaCadastro;
         String nome, email, senha;
 
         inputNomeCadastro = findViewById(R.id.inputNomeCadastro);
@@ -36,24 +42,57 @@ public class CadastroActivity extends AppCompatActivity {
         email = inputEmailCadastro.getText().toString();
         senha = inputSenhaCadastro.getText().toString();
 
-        Usuario usuario = new Usuario(nome, email, senha);
+        try {
+            postRequestCadastro("https://7pkqm4-37387.csb.app/cadastro", nome, email, senha);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        AlertDialog.Builder dadosCadastro = new AlertDialog.Builder(CadastroActivity.this);
-        dadosCadastro.setTitle("Cadastro Concluído com Sucesso!!!");
-        dadosCadastro.setMessage("Obrigado pelo seu cadastro!");
-        dadosCadastro.create().show();
+    }
 
-        new Handler().postDelayed(new Runnable() {
+    void postRequestCadastro(String postUrl, String nome, String email, String password) throws IOException {
+
+        RequestBody formBody = new FormBody.Builder()
+                .add("nome", nome)
+                .add("email", email)
+                .add("senha", password)
+                .build();
+
+        Request request = new Request.Builder()
+                .url(postUrl)
+                .post(formBody)
+                .build();
+
+        usuario.newCall(request).enqueue(new Callback() {
             @Override
-            public void run() {
-
-                Intent intent = new Intent(CadastroActivity.this, MainActivity.class);
-                startActivity(intent);
-
-                finish();
+            public void onFailure(Call call, IOException e) {
+                runOnUiThread(() -> Toast.makeText(CadastroActivity.this, "Erro na conexão com o servidor", Toast.LENGTH_SHORT).show());
             }
-        }, DELAY_TIME_MS);
 
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (!response.isSuccessful()) {
+                    runOnUiThread(() -> Toast.makeText(CadastroActivity.this, "Falha no cadastro", Toast.LENGTH_SHORT).show());
+                } else {
+                    runOnUiThread(() -> {
+                        Toast.makeText(CadastroActivity.this, "Cadastro bem-sucedido!", Toast.LENGTH_SHORT).show();
 
+                        AlertDialog.Builder dadosCadastro = new AlertDialog.Builder(CadastroActivity.this);
+                        dadosCadastro.setTitle("Cadastro Concluído com Sucesso!!!");
+                        dadosCadastro.setMessage("Obrigado pelo seu cadastro!");
+                        dadosCadastro.create().show();
+
+                        new Handler().postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                Intent intent = new Intent(CadastroActivity.this, LoginActivity.class);
+                                startActivity(intent);
+                                finish();
+                            }
+                        }, DELAY_TIME_MS);
+                    });
+                }
+            }
+        });
     }
 }
