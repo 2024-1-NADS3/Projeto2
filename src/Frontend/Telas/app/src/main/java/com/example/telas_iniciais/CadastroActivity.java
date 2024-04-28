e8a09fbe8a427df4e01a2af79a589176b0df4780
Kevin Shiroma <90286842+KevinShiroma@users.noreply.github.com>
2024-04-28 00:49:55 -0300

    conexao-bd

--- src/Frontend/Telas/app/src/main/java/com/example/telas_iniciais/CadastroActivity.java
+++ src/Frontend/Telas/app/src/main/java/com/example/telas_iniciais/CadastroActivity.java
@@ -7,22 +7,48 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
-
-import okhttp3.*;
 import android.widget.Toast;
-import java.io.IOException;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.SQLNonTransientConnectionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class CadastroActivity extends AppCompatActivity {
 
-    private static final long DELAY_TIME_MS = 3000;
-    private final OkHttpClient usuario = new OkHttpClient();
+    connection connectionClass;
+    Connection con;
+    ResultSet rs;
+    String name, str;
+
+
+    private EditText editTextNome;
+    private EditText editTextEmail;
+    private EditText editTextSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+        connectionClass = new connection();
+
+        editTextNome = findViewById(R.id.inputNomeCadastro);
+        editTextEmail = findViewById(R.id.inputEmailCadastro);
+        editTextSenha = findViewById(R.id.inputSenhaCadastro);
+
+        Button buttonCadastrar = findViewById(R.id.btnCadastrar);
+        buttonCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cadastrarUsuario();
+            }
+        });
     }
 
     public void voltarTelaLogin(View view) {
@@ -30,69 +56,104 @@ public class CadastroActivity extends AppCompatActivity {
         startActivity(voltarTelaLogin);
     }
 
-    public void BotaoCadastro(View view) {
-        EditText inputNomeCadastro, inputEmailCadastro, inputSenhaCadastro;
-        String nome, email, senha;
+    public static class MySQLConnection {
+        private static final String URL = "dbc:mysql://fecapsocialbd.mysql.database.azure.com:3306/fecapsocialdb";
+        private static final String USER = "fecapsocial@fecapsocialbd";
+        private static final String PASSWORD = "Hamburgada@";
 
-        inputNomeCadastro = findViewById(R.id.inputNomeCadastro);
-        inputEmailCadastro = findViewById(R.id.inputEmailCadastro);
-        inputSenhaCadastro = findViewById(R.id.inputSenhaCadastro);
+        public static Connection getConnection() throws SQLException {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                return DriverManager.getConnection(URL, USER, PASSWORD);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+                throw new SQLException("MySQL JDBC driver não encontrado.", e);
+            }
+        }
+    }
 
-        nome = inputNomeCadastro.getText().toString();
-        email = inputEmailCadastro.getText().toString();
-        senha = inputSenhaCadastro.getText().toString();
+    public class Cadastro {
+        private int id;
+        private String nome;
+        private String email;
+        private String senha;
 
-        try {
-            postRequestCadastro("https://7pkqm4-38883.csb.app/cadastro", nome, email, senha);
-        } catch (IOException e) {
-            e.printStackTrace();
+        public Cadastro(String nome, String email, String senha) {
+            this.nome = nome;
+            this.email = email;
+            this.senha = senha;
         }
 
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getSenha() {
+            return senha;
+        }
+
+        public void setSenha(String senha) {
+            this.senha = senha;
+        }
     }
 
-    void postRequestCadastro(String postUrl, String nome, String email, String password) throws IOException {
+    private void cadastrarUsuario() {
+        String nome = editTextNome.getText().toString().trim();
+        String email = editTextEmail.getText().toString().trim();
+        String senha = editTextSenha.getText().toString().trim();
 
-        RequestBody formBody = new FormBody.Builder()
-                .add("nome", nome)
-                .add("email", email)
-                .add("senha", password)
-                .build();
+        if (!nome.isEmpty() && !email.isEmpty() && !senha.isEmpty()) {
+            Cadastro cadastro = new Cadastro(nome, email, senha);
 
-        Request request = new Request.Builder()
-                .url(postUrl)
-                .post(formBody)
-                .build();
-
-        usuario.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                runOnUiThread(() -> Toast.makeText(CadastroActivity.this, "Erro na conexão com o servidor", Toast.LENGTH_SHORT).show());
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                if (!response.isSuccessful()) {
-                    runOnUiThread(() -> Toast.makeText(CadastroActivity.this, "Falha no cadastro", Toast.LENGTH_SHORT).show());
-                } else {
-                    runOnUiThread(() -> {
-                        Toast.makeText(CadastroActivity.this, "Cadastro bem-sucedido!", Toast.LENGTH_SHORT).show();
-
-                        AlertDialog.Builder dadosCadastro = new AlertDialog.Builder(CadastroActivity.this);
-                        dadosCadastro.setTitle("Cadastro Concluído com Sucesso!!!");
-                        dadosCadastro.setMessage("Obrigado pelo seu cadastro!");
-                        dadosCadastro.create().show();
-
-                        new Handler().postDelayed(new Runnable() {
-                            @Override
-                            public void run() {
-                                Intent intent = new Intent(CadastroActivity.this, LoginActivity.class);
-                                startActivity(intent);
-                                finish();
-                            }
-                        }, DELAY_TIME_MS);
-                    });
+            ExecutorService executorService = Executors.newSingleThreadExecutor();
+            executorService.execute(() -> {
+                try {
+                    con = connectionClass.CONN();
+                    if (con != null) {
+                        PreparedStatement statement = con.prepareStatement("INSERT INTO cadastro (nome, email, senha) VALUES (?, ?, ?)");
+                        statement.setString(1, cadastro.getNome());
+                        statement.setString(2, cadastro.getEmail());
+                        statement.setString(3, cadastro.getSenha());
+                        statement.executeUpdate();
+                        statement.close();
+                        str = "Usuário cadastrado com sucesso!";
+                    } else {
+                        str = "Erro na conexão";
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                    str = "Erro ao cadastrar usuário: " + e.getMessage();
                 }
-            }
-        });
+
+                runOnUiThread(() -> {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(this, str, Toast.LENGTH_LONG).show();
+                });
+            });
+        } else {
+            Toast.makeText(this, "Preencha todos os campos", Toast.LENGTH_SHORT).show();
+        }
     }
+
+
+
+
+
 }
+
