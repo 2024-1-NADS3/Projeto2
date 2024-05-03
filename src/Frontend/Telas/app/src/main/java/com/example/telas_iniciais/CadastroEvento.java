cd37b80ce46d1ea896ea9bc48b7deb77cf40102d
Fernanda Mayumi <128170082+Mayumikkato@users.noreply.github.com>
2024-05-03 13:34:19 -0300

    Commit - cadastroEvento

--- src/Frontend/Telas/app/src/main/java/com/example/telas_iniciais/CadastroEvento.java
+++ src/Frontend/Telas/app/src/main/java/com/example/telas_iniciais/CadastroEvento.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -19,6 +21,9 @@ import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CadastroEvento extends AppCompatActivity {
 
@@ -44,17 +49,20 @@ public class CadastroEvento extends AppCompatActivity {
         EditText inputEvento, inputData, inputCidade, inputLogradouro, inputNumero;
         String evento, data, cidade, logradouro, numero;
 
-        inputEvento = findViewById(R.id.editTextTextPersonName);
-        inputData = findViewById(R.id.editTextTextPersonName2);
-        inputCidade = findViewById(R.id.editTextTextPersonName3);
-        inputLogradouro = findViewById(R.id.editTextTextPersonName4);
-        inputNumero = findViewById(R.id.editTextTextPersonName5);
+        inputEvento = findViewById(R.id.evento);
+        inputData = findViewById(R.id.data);
+        inputCidade = findViewById(R.id.cidade);
+        inputLogradouro = findViewById(R.id.logradouro);
+        inputNumero = findViewById(R.id.numero);
 
         evento = inputEvento.getText().toString();
         data = inputData.getText().toString();
         cidade = inputCidade.getText().toString();
         logradouro = inputLogradouro.getText().toString();
         numero = inputNumero.getText().toString();
+
+        // Obter o idUsuarioLogado do SharedPreferences ou de onde você o armazenou
+        String idUsuarioLogado = obterIdUsuarioLogado();
 
         /*
          * Se o usuário deixar algum campo vazio exibe um AlertDialog de Erro
@@ -71,44 +79,26 @@ public class CadastroEvento extends AppCompatActivity {
 
         Evento eventoFecap = new Evento(evento, data, cidade, logradouro, numero);
 
-        String id_user = getIntent().getStringExtra("id_user");
-
-        eventoFecap.setId_user(id_user);
-
-        realizarCadastroEvento("https://twm93x-3000.csb.app/cadastroEvento", eventoFecap);
+        realizarCadastroEvento("https://twm93x-3000.csb.app/cadastroEvento", eventoFecap, idUsuarioLogado);
     }
 
-    void realizarCadastroEvento(String postUrl, final Evento evento) {
-        JSONObject jsonBody = new JSONObject();
-        try {
-            jsonBody.put("evento", evento.getEvento());
-            jsonBody.put("data", evento.getData());
-            jsonBody.put("cidade", evento.getCidade());
-            jsonBody.put("logradouro", evento.getLogradouro());
-            jsonBody.put("numero", evento.getNumero());
-            jsonBody.put("id_user", evento.getId_user());
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+    void realizarCadastroEvento(String postUrl, final Evento evento, String idUsuarioLogado) {
+        Map<String, String> dadosEvento = new HashMap<>();
+        dadosEvento.put("evento", evento.getEvento());
+        dadosEvento.put("data", evento.getData());
+        dadosEvento.put("cidade", evento.getCidade());
+        dadosEvento.put("logradouro", evento.getLogradouro());
+        dadosEvento.put("numero", evento.getNumero());
+        dadosEvento.put("idUsuarioLogado", idUsuarioLogado);
 
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, postUrl, jsonBody,
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, postUrl, new JSONObject(dadosEvento),
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
+                        // Aqui, 'response' é a resposta do servidor como um objeto JSON
+                        // Você pode processá-la conforme necessário
                         try {
-                            // Obtém a mensagem da resposta
-                            String message = response.getString("message");
-
-                            if (message.equals("Evento já cadastrado!")) {
-                                Toast.makeText(CadastroEvento.this, "Evento já cadastrado!", Toast.LENGTH_SHORT).show();
-
-                                AlertDialog.Builder dadosCadastro = new AlertDialog.Builder(CadastroEvento.this);
-                                dadosCadastro.setTitle("Erro!!!");
-                                dadosCadastro.setMessage("Evento já cadastrado!");
-                                dadosCadastro.setIcon(android.R.drawable.ic_dialog_alert);
-                                dadosCadastro.setNegativeButton("Tentar Novamente", null);
-                                dadosCadastro.create().show();
-                            } else {
+                            if (response.getString("status").equals("success")) {
                                 // Registro bem-sucedido
                                 Toast.makeText(CadastroEvento.this, "Cadastro do evento bem-sucedido!", Toast.LENGTH_SHORT).show();
 
@@ -126,20 +116,35 @@ public class CadastroEvento extends AppCompatActivity {
                                         finish();
                                     }
                                 }, tempoDelayParaMudarTela);
+                            } else {
+                                // Exibir mensagem de erro
+                                String mensagemErro = response.getString("message");
+                                Toast.makeText(CadastroEvento.this, mensagemErro, Toast.LENGTH_SHORT).show();
                             }
                         } catch (JSONException e) {
                             e.printStackTrace();
+                            Toast.makeText(CadastroEvento.this, "Erro ao processar resposta do servidor", Toast.LENGTH_SHORT).show();
                         }
                     }
                 },
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
+                        // Imprime a mensagem de erro
+                        Log.e("VolleyError", error.toString());
+
+                        // Imprime a pilha de chamadas
+                        error.printStackTrace();
                         // Falha no registro
                         Toast.makeText(CadastroEvento.this, "Erro ao cadastrar o evento", Toast.LENGTH_SHORT).show();
                     }
                 });
 
         filaRequest.add(jsonObjectRequest);
+    }
+
+    public String obterIdUsuarioLogado() {
+        SharedPreferences sharedPreferences = getSharedPreferences("PreferenciaUsuario", MODE_PRIVATE);
+        return sharedPreferences.getString("id_user", "");
     }
 }
